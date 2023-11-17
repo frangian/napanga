1c3b9bf9e732e01b863afa3342000c33b3522dee
frangian <franciscogianzanti@gmail.com>
2023-11-17 19:06:24 -0300

    se agrego type

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,30 +7,35 @@ export const routes = [
   {
     id: "home",
     title: "Home",
+    type: 'pages',
     path: "/",
     Element: HomeContainer
   },
   {
     id: "products",
-    title: "Products",
+    title: "Productos",
+    type: 'pages',
     path: "/products",
     Element: ItemListContainer,
   },
   {
     id: "category",
     title: "Category",
+    type: 'link',
     path: "/products/:categoryId",
     Element: ItemListContainer,
   },
   {
     id: "contact",
-    title: "Contact",
+    title: "Contacto",
+    type: 'pages',
     path: "/contact",
     Element: ContactContainer,
   },
   {
     id: "item",
     title: "Item",
+    type: 'link',
     path: "/item/:itemId",
     Element: ItemContainer,
   }
