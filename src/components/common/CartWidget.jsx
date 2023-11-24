2e73a878e72542160c70deb79282841a8ac63e62
frangian <franciscogianzanti@gmail.com>
2023-11-24 13:08:46 -0300

    cartWidget v2

--- src/components/common/CartWidget.jsx
+++ src/components/common/CartWidget.jsx
@@ -1,11 +1,30 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useCart } from "../../context/CartContext"
 
-const CartWidget = () => {
+const CartWidget = ( ) => {
+  const { itemsQuantity } = useCart();
+   
   return (
-    <div>
-      <FontAwesomeIcon icon={faShoppingCart} />
-      <span style={{ color: 'white' }}> 0</span>
+    <div style={{ position: "relative" }}>
+      <ShoppingCartIcon style={{ color: "#72A184" }} />
+      <span
+        style={{
+          position: "absolute",
+          left: 14,
+          right: 14,
+          backgroundColor: "#72A184",
+          width: 16,
+          height: 14,
+          borderRadius: 7,
+          fontFamily: "Roboto",
+          fontSize: 12,
+          fontWeight: 500,
+          textAlign: "center",
+          color: "#2A332D",
+        }}
+      >
+        {itemsQuantity}
+      </span>
     </div>
   );
 };
