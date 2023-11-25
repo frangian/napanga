c06afc2bfb8115c232b6100e1beb6713eb109e10
frangian <franciscogianzanti@gmail.com>
2023-11-25 18:11:46 -0300

    cartWidget: se muestra o no la burbuja

--- src/components/common/CartWidget.jsx
+++ src/components/common/CartWidget.jsx
@@ -1,30 +1,36 @@
+import { useCart } from "../../context/CartContext";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { useCart } from "../../context/CartContext"
+import { customTheme } from "../../themeconfig";
 
-const CartWidget = ( ) => {
+const CartWidget = () => {
+  console.log("cartwidget render");
   const { itemsQuantity } = useCart();
-   
+
+  const Bubble = ({ children }) => (
+    <span
+      style={{
+        position: "absolute",
+        left: 14,
+        right: 14,
+        backgroundColor: customTheme.palette.green.light,
+        width: 16,
+        height: 14,
+        borderRadius: 7,
+        fontFamily: "Roboto",
+        fontSize: 12,
+        fontWeight: 500,
+        textAlign: "center",
+        color: customTheme.palette.green.dark,
+      }}
+    >
+      {children}
+    </span>
+  );
+
   return (
     <div style={{ position: "relative" }}>
-      <ShoppingCartIcon style={{ color: "#72A184" }} />
-      <span
-        style={{
-          position: "absolute",
-          left: 14,
-          right: 14,
-          backgroundColor: "#72A184",
-          width: 16,
-          height: 14,
-          borderRadius: 7,
-          fontFamily: "Roboto",
-          fontSize: 12,
-          fontWeight: 500,
-          textAlign: "center",
-          color: "#2A332D",
-        }}
-      >
-        {itemsQuantity}
-      </span>
+      <ShoppingCartIcon style={{ color: customTheme.palette.green.light }} />
+      {itemsQuantity !== 0 ? <Bubble>{itemsQuantity}</Bubble> : ""}
     </div>
   );
 };
